33ce031afc0ddf1db0d81308affef8bb91899b03
Brian La <133820463+bla29@users.noreply.github.com>
2025-12-10 13:08:36 -0800

    delete company working

--- cv-application/src/components/DisplayCompany.jsx
+++ cv-application/src/components/DisplayCompany.jsx
@@ -1,6 +1,12 @@
 import '../styles/DisplayCompany.css'
 
-export default function DisplayCompany({ value }) {
+export default function DisplayCompany({ value, setValue }) {
+
+    function handleDelete(id) {
+        let newValue = value.filter((value) => value.id != id);
+        setValue(newValue);
+    }
+
     return (
         <div className='section'>
             <h2>Companies:</h2>
@@ -11,6 +17,7 @@ export default function DisplayCompany({ value }) {
                     <p>{value.responsibilities}</p>
                     <p>{value.fromdate}</p>
                     <p>{value.todate}</p>
+                    <button onClick={() => handleDelete(value.id)}>Delete</button>
                 </div>
             ))}
         </div>
